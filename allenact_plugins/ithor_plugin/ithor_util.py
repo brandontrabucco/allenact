4d01ab645e35a8ebe219c416cf6ac6b68ca3008e
Brandon Trabucco <brandon@btrabucco.com>
2021-12-24 10:27:29 -0500

    Update ithor_util.py

--- allenact_plugins/ithor_plugin/ithor_util.py
+++ allenact_plugins/ithor_plugin/ithor_util.py
@@ -62,18 +62,22 @@ def get_open_x_displays(throw_error_if_empty: bool = False) -> Sequence[str]:
     assert platform.system() == "Linux", "Can only get X-displays for Linux systems."
 
     displays = []
-
+    
     open_display_strs = [
         os.path.basename(s)[1:] for s in glob.glob("/tmp/.X11-unix/X*")
     ]
 
     for open_display_str in sorted(open_display_strs):
+
         try:
             open_display_str = str(int(open_display_str))
         except Exception:
             continue
 
-        display = Xlib.display.Display(":{}".format(open_display_str))
+        try:
+            display = Xlib.display.Display(":{}".format(open_display_str))
+        except Xlib.error.DisplayConnectionError:
+            continue
 
         displays.extend(
             [f"{open_display_str}.{i}" for i in range(display.screen_count())]
