f2a123e66175ef94daf98e070348fcf00aa079d7
Brandon Trabucco <brandon@btrabucco.com>
2022-02-04 11:53:32 -0500

    adding startx change

--- scripts/startx.py
+++ scripts/startx.py
@@ -1,33 +1,29 @@
-import atexit
-import os
-import platform
-import re
-import shlex
+#!/usr/bin/env python
+
 import subprocess
+import shlex
+import re
+import platform
 import tempfile
+import os
+import sys
 
-
-# Turning off automatic black formatting for this script as it breaks quotes.
-
-# fmt: off
 
 def pci_records():
     records = []
-    command = shlex.split("lspci -vmm")
+    command = shlex.split('lspci -vmm')
     output = subprocess.check_output(command).decode()
-
     for devices in output.strip().split("\n\n"):
         record = {}
         records.append(record)
         for row in devices.split("\n"):
             key, value = row.split("\t")
-            record[key.split(":")[0]] = value
-
+            record[key.split(':')[0]] = value
     return records
+
 
 def generate_xorg_conf(devices):
     xorg_conf = []
-
     device_section = """
 Section "Device"
     Identifier     "Device{device_id}"
@@ -59,40 +55,37 @@ EndSection
         xorg_conf.append(device_section.format(device_id=i, bus_id=bus_id))
         xorg_conf.append(screen_section.format(device_id=i, screen_id=i))
         screen_records.append('Screen {screen_id} "Screen{screen_id}" 0 0'.format(screen_id=i))
-    
     xorg_conf.append(server_layout_section.format(screen_records="\n    ".join(screen_records)))
-
     output =  "\n".join(xorg_conf)
+    print(output)
     return output
 
-def startx(display=0):
-    if platform.system() != "Linux":
-        raise Exception("Can only run startx on linux")
 
+def startx(display):
+    if platform.system() != 'Linux':
+        raise Exception("Can only run startx on linux")
     devices = []
     for r in pci_records():
-        if r.get("Vendor", "") == "NVIDIA Corporation"\
-                and r["Class"] in ["VGA compatible controller", "3D controller"]:
-            bus_id = "PCI:" + ":".join(map(lambda x: str(int(x, 16)), re.split(r"[:\.]", r["Slot"])))
+        if r.get('Vendor', '') == 'NVIDIA Corporation' \
+                and r['Class'] in ['VGA compatible controller', '3D controller']:
+            bus_id = 'PCI:' + ':'.join(map(lambda x: str(int(x, 16)), re.split(r'[:\.]', r['Slot'])))
             devices.append(bus_id)
-
     if not devices:
         raise Exception("no nvidia cards found")
-
     try:
-        fd, path = tempfile.mkstemp()
+        path = 'temp'; print("path is ", path)
         with open(path, "w") as f:
             f.write(generate_xorg_conf(devices))
         command = shlex.split("Xorg -noreset +extension GLX +extension RANDR +extension RENDER -config %s :%s" % (path, display))
-        proc = subprocess.Popen(command)
-        atexit.register(lambda: proc.poll() is None and proc.kill())
-        proc.wait()
-    finally: 
-        os.close(fd)
+        subprocess.call(command)
+    finally:
+        #os.close(fd)
         os.unlink(path)
 
-# fmt: on
 
-
-if __name__ == "__main__":
-    startx()
+if __name__ == '__main__':
+    display = 0
+    if len(sys.argv) > 1:
+        display = int(sys.argv[1])
+    print("Starting X on DISPLAY=:%s" % display)
+    startx(display)
